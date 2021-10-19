b8417ebc0de53d3cc7264b7d1e487f3c066f569e
Sabotage84 <sabotage84@mail.ru>
2021-10-19 22:32:12 +0300

    ADD type_prod and Prod abstract class

---
+++ OfferHelperV1/OfferHelperV1/ProductClasses/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfferHelperV1.ProductClasses
+{
+    abstract class Product
+    {
+        int iD;
+        string name;
+        string description;
+        int inPrice;
+        double k;
+        int price;
+        TypeOfProduct type;
+
+        public string Name { get => name; set => name = value; }
+        public int ID { get => iD; set => iD = value; }
+        public string Description { get => description; set => description = value; }
+        public int InPrice { get => inPrice; set => inPrice = value; }
+        public double K { get => k; set => k = value; }
+        public int Price { get => price; set => price = value; }
+        internal TypeOfProduct Type { get => type; set => type = value; }
+    }
+}
