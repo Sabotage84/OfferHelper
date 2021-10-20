b7f7da3a1b3db9e0de8b3f7b45fc3635325b1b17
Sabotage84 <sabotage84@mail.ru>
2021-10-20 19:48:09 +0300

    ADD server class

---
+++ OfferHelperV1/OfferHelperV1/ProductClasses/Server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfferHelperV1.ProductClasses
+{
+    class Server:Product
+    {
+        public Server()
+        {
+            Type = TypeOfProduct.Server;
+        }
+    }
+}
