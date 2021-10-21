b23eefc542f8f31d72ab56c90da94f3c1f77e2d8
Sabotage84 <sabotage84@mail.ru>
2021-10-21 22:36:22 +0300

    ADD antenna class

---
+++ OfferHelperV1/OfferHelperV1/ProductClasses/Antenna.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfferHelperV1.ProductClasses
+{
+    class Antenna:Product
+    {
+        public Antenna()
+        {
+            Type = TypeOfProduct.Antenna;
+        }
+    }
+}
