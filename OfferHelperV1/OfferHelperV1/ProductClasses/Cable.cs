f1af7c72f44902164dd382fa986d2d027f921447
Sabotage84 <sabotage84@mail.ru>
2021-10-22 23:33:58 +0300

    ADD cable class

---
+++ OfferHelperV1/OfferHelperV1/ProductClasses/Cable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfferHelperV1.ProductClasses
+{
+    class Cable:Product
+    {
+        public Cable()
+        {
+            Type = TypeOfProduct.Cable;
+        }
+    }
+}
