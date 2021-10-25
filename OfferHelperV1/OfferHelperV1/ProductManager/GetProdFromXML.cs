1b6626291ae71c9f9e7512d5aae51acb5ddaa200
Sabotage84 <sabotage84@mail.ru>
2021-10-25 21:59:25 +0300

    ADD manager

---
+++ OfferHelperV1/OfferHelperV1/ProductManager/GetProdFromXML.cs
@@ -0,0 +1,23 @@
+﻿using OfferHelperV1.ProductClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfferHelperV1.ProductManager
+{
+    class GetProdFromXML
+    {
+        List<Product> products = new List<Product>();
+        public GetProdFromXML()
+        {
+            products.Add(new Server());
+        }
+
+        public List<Product> AllProducts()
+        {
+            return products;
+        }
+    }
+}
---
+++ OfferHelperV1/OfferHelperV1/ProductManager/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using OfferHelperV1.ProductClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfferHelperV1.ProductManager
+{
+    class ProductManager
+    {
+        List<Product> products = new List<Product>();
+        public ProductManager()
+        {
+            products = GetProducts();
+        }
+
+        private List<Product> GetProducts()
+        {
+            GetProdFromXML t = new GetProdFromXML();
+            return t.AllProducts();
+        }
+    }
+}
