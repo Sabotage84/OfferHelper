d91d7b5b0ad595b7f60b475940baf61471a41ea1
Sabotage84 <sabotage84@mail.ru>
2021-10-27 21:42:47 +0300

    ADD manager getBID getSameName

--- OfferHelperV1/OfferHelperV1/ProductManager/ProductManager.cs
+++ OfferHelperV1/OfferHelperV1/ProductManager/ProductManager.cs
@@ -25,5 +25,28 @@ namespace OfferHelperV1.ProductManager
         {
             GetProdFromXML.Save(prods);
         }
+
+        public Product GetProductByID(int id)
+        {
+            foreach (var item in products)
+            {
+                if (item.ID == id)
+                    return item;
+            }
+            return null;
+        }
+
+        public List<Product> GetSameNaneProducts(string str)
+        {
+            List<Product> res = new List<Product>();
+            foreach (var item in products)
+            {
+                if (item.Name.Contains(str))
+                {
+                    res.Add(item);
+                }
+            }
+            return res;
+        }
     }
 }
