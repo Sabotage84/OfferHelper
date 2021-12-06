17e2a7a79c7da335c3692569e72df9122fc194d1
Sabotage84 <sabotage84@mail.ru>
2021-12-07 00:08:58 +0300

    ADD text template class2

--- OfferHelperV1/OfferHelperV1/OfferClasses/TextTemplates.cs
+++ OfferHelperV1/OfferHelperV1/OfferClasses/TextTemplates.cs
@@ -24,7 +24,32 @@ namespace OfferHelperV1.OfferClasses
                           Ознакомьтесь, пожалуйста, с памяткой:
                             http://www.ptime.ru/Downloads/pamyatka_Metronom.pdf";
 
+        string verificationExample = @"В качестве примера, если нужен Метроном-300 с поверкой, то это будет:
+                                        Устройство синхронизации частоты и времени Метроном-300/GNS-HQ
+                                        2 x NTP LAN Ethernet 10/100, RJ45; 2xRS232, 1x10МГц (TTL), 1x1PPS, генератор
+                                        OCXO-HQ, эл.пит. ~220В.
+                                        6300 Евро с НДС.
+                                        Антенна ГЛОНАСС/GPS Радиус-50 со встроенным грозоразрядником.
+                                        435 Евро с НДС.
+                                        Антенный кабель 50 м.
+                                        140 Евро с НДС.
+                                        Организация поверки (получение свидетельства о поверке)
+                                        500 Евро с НДС.
+
+                                        http://www.ptime.ru/Metronom/si/Metronom300si.html
+                                        Оплата в рублях по курсу ЦБ.
+                                        Склад Москва 14-16 недель (12 недель + 2-4 недели поверка).
+                                        3 года гарантии. (Расширение гарантии до 5 лет +20% к стоимости)
+                                        Паспорт на изделие. Декларация соответствия ТС (EAC). Свидетельство о поверке.";
+
+        string deliveryText = @"Оплата в рублях по курсу ЦБ.
+                                Склад Москва 10-11 недель. Бывает на складе.
+                                3 года гарантии. (Расширение гарантии до 5 лет +20% к стоимости).
+                                Паспорт на изделие. Декларация соответствия ТС (EAC).";
+
         public string Producer { get => producer; set => producer = value; }
         public string Remark { get => remark; set => remark = value; }
+        public string VerificationExample { get => verificationExample; set => verificationExample = value; }
+        public string DeliveryText { get => deliveryText; set => deliveryText = value; }
     }
 }
