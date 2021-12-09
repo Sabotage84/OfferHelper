d41485a1e2a172a68407a7476df87a10c86c0978
Sabotage84 <sabotage84@mail.ru>
2021-12-09 21:48:45 +0300

    ADD text template form2

--- OfferHelperV1/OfferHelperV1/EditTextTemplates.Designer.cs
+++ OfferHelperV1/OfferHelperV1/EditTextTemplates.Designer.cs
@@ -29,12 +29,44 @@ namespace OfferHelperV1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Producer_lbl = new System.Windows.Forms.Label();
+            this.Producer_txtBx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Producer_lbl
+            // 
+            this.Producer_lbl.AutoSize = true;
+            this.Producer_lbl.Location = new System.Drawing.Point(22, 28);
+            this.Producer_lbl.Name = "Producer_lbl";
+            this.Producer_lbl.Size = new System.Drawing.Size(110, 17);
+            this.Producer_lbl.TabIndex = 0;
+            this.Producer_lbl.Text = "Производитель";
+            // 
+            // Producer_txtBx
+            // 
+            this.Producer_txtBx.Location = new System.Drawing.Point(25, 48);
+            this.Producer_txtBx.Multiline = true;
+            this.Producer_txtBx.Name = "Producer_txtBx";
+            this.Producer_txtBx.Size = new System.Drawing.Size(230, 83);
+            this.Producer_txtBx.TabIndex = 1;
+            // 
+            // EditTextTemplates
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(860, 477);
+            this.Controls.Add(this.Producer_txtBx);
+            this.Controls.Add(this.Producer_lbl);
+            this.Name = "EditTextTemplates";
             this.Text = "EditTextTemplates";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Producer_lbl;
+        private System.Windows.Forms.TextBox Producer_txtBx;
     }
 }
