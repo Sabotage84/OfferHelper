f320fbaa2d95062c9bc19cc5aabd21c216c501e0
Sabotage84 <sabotage84@mail.ru>
2021-12-28 23:02:44 +0300

    ADD offer list class not done

---
+++ OfferHelperV1/OfferHelperV1/OfferClasses/OfferList.cs
@@ -0,0 +1,24 @@
+﻿using OfferHelperV1.ProductClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfferHelperV1.OfferClasses
+{
+    class OfferList
+    {
+        List<Product> items = new List<Product>();
+
+        public void AddItem(Product p)
+        {
+            items.Add(p);
+        }
+
+        public void RemoveItem(Product p)
+        {
+            items.Remove(p);
+        }
+    }
+}
