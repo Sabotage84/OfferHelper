05c07f42ce784ca1005d40328dcf29e7682cec3e
Sabotage84 <sabotage84@mail.ru>
2022-01-14 17:53:51 +0300

    ADD edit prof form v1

--- OfferHelperV1/OfferHelperV1/EditProductForm.Designer.cs
+++ OfferHelperV1/OfferHelperV1/EditProductForm.Designer.cs
@@ -29,12 +29,223 @@ namespace OfferHelperV1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Name_lbl = new System.Windows.Forms.Label();
+            this.Name_txtBx = new System.Windows.Forms.TextBox();
+            this.ID_lbl = new System.Windows.Forms.Label();
+            this.ID_txtBx = new System.Windows.Forms.TextBox();
+            this.Description_lbl = new System.Windows.Forms.Label();
+            this.Description_txtBx = new System.Windows.Forms.TextBox();
+            this.InPrice_lbl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.K_lbl = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Price_lbl = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Type_lbl = new System.Windows.Forms.Label();
+            this.Type_cmbBx = new System.Windows.Forms.ComboBox();
+            this.DeliveryTime_lbl = new System.Windows.Forms.Label();
+            this.DeliveryTime_txtBx = new System.Windows.Forms.TextBox();
+            this.Save_btn = new System.Windows.Forms.Button();
+            this.Cancel_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Name_lbl
+            // 
+            this.Name_lbl.AutoSize = true;
+            this.Name_lbl.Location = new System.Drawing.Point(25, 24);
+            this.Name_lbl.Name = "Name_lbl";
+            this.Name_lbl.Size = new System.Drawing.Size(35, 17);
+            this.Name_lbl.TabIndex = 0;
+            this.Name_lbl.Text = "Имя";
+            // 
+            // Name_txtBx
+            // 
+            this.Name_txtBx.Location = new System.Drawing.Point(28, 44);
+            this.Name_txtBx.Name = "Name_txtBx";
+            this.Name_txtBx.Size = new System.Drawing.Size(272, 22);
+            this.Name_txtBx.TabIndex = 1;
+            // 
+            // ID_lbl
+            // 
+            this.ID_lbl.AutoSize = true;
+            this.ID_lbl.Location = new System.Drawing.Point(25, 92);
+            this.ID_lbl.Name = "ID_lbl";
+            this.ID_lbl.Size = new System.Drawing.Size(21, 17);
+            this.ID_lbl.TabIndex = 2;
+            this.ID_lbl.Text = "ID";
+            // 
+            // ID_txtBx
+            // 
+            this.ID_txtBx.Location = new System.Drawing.Point(28, 112);
+            this.ID_txtBx.Name = "ID_txtBx";
+            this.ID_txtBx.Size = new System.Drawing.Size(272, 22);
+            this.ID_txtBx.TabIndex = 3;
+            // 
+            // Description_lbl
+            // 
+            this.Description_lbl.AutoSize = true;
+            this.Description_lbl.Location = new System.Drawing.Point(25, 164);
+            this.Description_lbl.Name = "Description_lbl";
+            this.Description_lbl.Size = new System.Drawing.Size(74, 17);
+            this.Description_lbl.TabIndex = 4;
+            this.Description_lbl.Text = "Описание";
+            // 
+            // Description_txtBx
+            // 
+            this.Description_txtBx.Location = new System.Drawing.Point(28, 184);
+            this.Description_txtBx.Multiline = true;
+            this.Description_txtBx.Name = "Description_txtBx";
+            this.Description_txtBx.Size = new System.Drawing.Size(438, 120);
+            this.Description_txtBx.TabIndex = 5;
+            // 
+            // InPrice_lbl
+            // 
+            this.InPrice_lbl.AutoSize = true;
+            this.InPrice_lbl.Location = new System.Drawing.Point(525, 24);
+            this.InPrice_lbl.Name = "InPrice_lbl";
+            this.InPrice_lbl.Size = new System.Drawing.Size(99, 17);
+            this.InPrice_lbl.TabIndex = 6;
+            this.InPrice_lbl.Text = "Входная цена";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(528, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // K_lbl
+            // 
+            this.K_lbl.AutoSize = true;
+            this.K_lbl.Location = new System.Drawing.Point(525, 92);
+            this.K_lbl.Name = "K_lbl";
+            this.K_lbl.Size = new System.Drawing.Size(101, 17);
+            this.K_lbl.TabIndex = 8;
+            this.K_lbl.Text = "Коэффициент";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(528, 112);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(130, 22);
+            this.textBox3.TabIndex = 10;
+            // 
+            // Price_lbl
+            // 
+            this.Price_lbl.AutoSize = true;
+            this.Price_lbl.Location = new System.Drawing.Point(525, 164);
+            this.Price_lbl.Name = "Price_lbl";
+            this.Price_lbl.Size = new System.Drawing.Size(43, 17);
+            this.Price_lbl.TabIndex = 11;
+            this.Price_lbl.Text = "Цена";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(528, 184);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(130, 22);
+            this.textBox4.TabIndex = 12;
+            // 
+            // Type_lbl
+            // 
+            this.Type_lbl.AutoSize = true;
+            this.Type_lbl.Location = new System.Drawing.Point(342, 24);
+            this.Type_lbl.Name = "Type_lbl";
+            this.Type_lbl.Size = new System.Drawing.Size(33, 17);
+            this.Type_lbl.TabIndex = 13;
+            this.Type_lbl.Text = "Тип";
+            // 
+            // Type_cmbBx
+            // 
+            this.Type_cmbBx.FormattingEnabled = true;
+            this.Type_cmbBx.Location = new System.Drawing.Point(345, 44);
+            this.Type_cmbBx.Name = "Type_cmbBx";
+            this.Type_cmbBx.Size = new System.Drawing.Size(121, 24);
+            this.Type_cmbBx.TabIndex = 14;
+            // 
+            // DeliveryTime_lbl
+            // 
+            this.DeliveryTime_lbl.AutoSize = true;
+            this.DeliveryTime_lbl.Location = new System.Drawing.Point(525, 231);
+            this.DeliveryTime_lbl.Name = "DeliveryTime_lbl";
+            this.DeliveryTime_lbl.Size = new System.Drawing.Size(104, 17);
+            this.DeliveryTime_lbl.TabIndex = 15;
+            this.DeliveryTime_lbl.Text = "Срок поставки";
+            // 
+            // DeliveryTime_txtBx
+            // 
+            this.DeliveryTime_txtBx.Location = new System.Drawing.Point(528, 251);
+            this.DeliveryTime_txtBx.Name = "DeliveryTime_txtBx";
+            this.DeliveryTime_txtBx.Size = new System.Drawing.Size(130, 22);
+            this.DeliveryTime_txtBx.TabIndex = 16;
+            // 
+            // Save_btn
+            // 
+            this.Save_btn.Location = new System.Drawing.Point(456, 335);
+            this.Save_btn.Name = "Save_btn";
+            this.Save_btn.Size = new System.Drawing.Size(95, 31);
+            this.Save_btn.TabIndex = 17;
+            this.Save_btn.Text = "Сохранить";
+            this.Save_btn.UseVisualStyleBackColor = true;
+            // 
+            // Cancel_btn
+            // 
+            this.Cancel_btn.Location = new System.Drawing.Point(563, 335);
+            this.Cancel_btn.Name = "Cancel_btn";
+            this.Cancel_btn.Size = new System.Drawing.Size(95, 31);
+            this.Cancel_btn.TabIndex = 18;
+            this.Cancel_btn.Text = "Отмена";
+            this.Cancel_btn.UseVisualStyleBackColor = true;
+            // 
+            // EditProductForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(699, 392);
+            this.Controls.Add(this.Cancel_btn);
+            this.Controls.Add(this.Save_btn);
+            this.Controls.Add(this.DeliveryTime_txtBx);
+            this.Controls.Add(this.DeliveryTime_lbl);
+            this.Controls.Add(this.Type_cmbBx);
+            this.Controls.Add(this.Type_lbl);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Price_lbl);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.K_lbl);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.InPrice_lbl);
+            this.Controls.Add(this.Description_txtBx);
+            this.Controls.Add(this.Description_lbl);
+            this.Controls.Add(this.ID_txtBx);
+            this.Controls.Add(this.ID_lbl);
+            this.Controls.Add(this.Name_txtBx);
+            this.Controls.Add(this.Name_lbl);
+            this.Name = "EditProductForm";
             this.Text = "EditProductForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Name_lbl;
+        private System.Windows.Forms.TextBox Name_txtBx;
+        private System.Windows.Forms.Label ID_lbl;
+        private System.Windows.Forms.TextBox ID_txtBx;
+        private System.Windows.Forms.Label Description_lbl;
+        private System.Windows.Forms.TextBox Description_txtBx;
+        private System.Windows.Forms.Label InPrice_lbl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label K_lbl;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label Price_lbl;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label Type_lbl;
+        private System.Windows.Forms.ComboBox Type_cmbBx;
+        private System.Windows.Forms.Label DeliveryTime_lbl;
+        private System.Windows.Forms.TextBox DeliveryTime_txtBx;
+        private System.Windows.Forms.Button Save_btn;
+        private System.Windows.Forms.Button Cancel_btn;
     }
 }
